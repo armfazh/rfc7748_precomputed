c71ebc824b0b57f526083617e3f6ce753499010e
armfazh <armfazh@gmail.com>
2019-11-30 04:15:35 -0800

    Getting fuzz relying on clang

--- fuzz/gmp_fp448_add.c
+++ fuzz/gmp_fp448_add.c
@@ -14,14 +14,14 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 #include <assert.h>
-#include <fp25519_x64.h>
+#include <fp448_x64.h>
 #include <gmp.h>
 #include <string.h>
 
 /**
- * Verifies that 0 <= c=a+b < 2^256 and that c be congruent to a+b mod p
+ * Verifies that 0 <= c=a+b < 2^448 and that c be congruent to a+b mod p
  * @param Data Random binary data.
  * @param Size Non-trivial input size is set -max_len=64
  * @return Always return 0 in case of success.
@@ -31,47 +31,52 @@ extern "C" {
 #endif
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  if (Size != 2 * 32) return -1;
+  if (Size != 2 * SIZE_BYTES_FP448)
+    return -1;
 
-  EltFp25519_1w_x64 a, b, get_c, want_c;
+  EltFp448_1w_x64 a, b, get_c, want_c;
 
-  mpz_t gmp_a, gmp_b, gmp_c, two_prime, two_to_256;
+  mpz_t gmp_a, gmp_b, gmp_c, prime, two_to_448;
   mpz_init(gmp_a);
   mpz_init(gmp_b);
   mpz_init(gmp_c);
 
-  // // two_prime = 2^256-38
-  // mpz_init_set_ui(two_prime, 1);
-  // mpz_mul_2exp(two_prime, two_prime, 256);
-  // mpz_sub_ui(two_prime, two_prime, 38);
+  // prime = 2^448-2^224-1
+  mpz_init_set_ui(prime, 1);
+  mpz_mul_2exp(prime, prime, 224);
+  mpz_sub_ui(prime, prime, 1);
+  mpz_mul_2exp(prime, prime, 224);
+  mpz_sub_ui(prime, prime, 1);
 
-  // two_to_256 = 2^256
-  mpz_init_set_ui(two_to_256, 1);
-  mpz_mul_2exp(two_to_256, two_to_256, 256);
+  // two_to_448 = 2^448
+  mpz_init_set_ui(two_to_448, 1);
+  mpz_mul_2exp(two_to_448, two_to_448, 448);
 
-  memcpy(a, Data + 0, 32);
-  memcpy(b, Data + 32, 32);
+  memcpy(a, Data + 0 * SIZE_BYTES_FP448, SIZE_BYTES_FP448);
+  memcpy(b, Data + 1 * SIZE_BYTES_FP448, SIZE_BYTES_FP448);
 
-  setzero_EltFp25519_1w_x64(get_c);
-  setzero_EltFp25519_1w_x64(want_c);
+  setzero_EltFp448_1w_x64(get_c);
+  setzero_EltFp448_1w_x64(want_c);
 
-  add_EltFp25519_1w_x64(get_c, a, b);
+  add_EltFp448_1w_x64(get_c, a, b);
 
-  mpz_import(gmp_a, SIZE_BYTES_FP25519, -1, sizeof(Data[0]), 0, 0, Data + 0);
-  mpz_import(gmp_b, SIZE_BYTES_FP25519, -1, sizeof(Data[0]), 0, 0, Data + 32);
+  mpz_import(gmp_a, SIZE_BYTES_FP448, -1, sizeof(Data[0]), 0, 0,
+             Data + 0 * SIZE_BYTES_FP448);
+  mpz_import(gmp_b, SIZE_BYTES_FP448, -1, sizeof(Data[0]), 0, 0,
+             Data + 1 * SIZE_BYTES_FP448);
 
   mpz_add(gmp_c, gmp_a, gmp_b);
-  while (mpz_cmp(gmp_c, two_to_256) >= 0) {
-    mpz_sub(gmp_c, gmp_c, two_prime);
+  while (mpz_cmp(gmp_c, two_to_448) >= 0) {
+    mpz_sub(gmp_c, gmp_c, prime);
   }
-  mpz_export(want_c, NULL, -1, SIZE_BYTES_FP25519, 0, 0, gmp_c);
-  assert(memcmp(get_c, want_c, SIZE_BYTES_FP25519) == 0);
+  mpz_export(want_c, NULL, -1, SIZE_BYTES_FP448, 0, 0, gmp_c);
+  assert(memcmp(get_c, want_c, SIZE_BYTES_FP448) == 0);
 
   mpz_clear(gmp_a);
   mpz_clear(gmp_b);
   mpz_clear(gmp_c);
-  // mpz_clear(two_prime);
-  mpz_clear(two_to_256);
+  mpz_clear(prime);
+  mpz_clear(two_to_448);
   return 0;
 }
 
